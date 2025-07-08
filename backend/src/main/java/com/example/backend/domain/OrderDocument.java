7078528f8b813900eacbfe8aad4b850c9e43ea8d
symicy <alexsima141@gmail.com>
2025-07-08 20:48:32 +0300

    creare tabele
    Co-authored-by: Vancea Paul <Vancea-Paul@users.noreply.github.com>
    Co-authored-by: Andrei Tuns <markoqaq@users.noreply.github.com>
    Co-authored-by: DenisBachmatchi12 <DenisBachmatchi12@users.noreply.github.com>

---
+++ backend/src/main/java/com/example/backend/domain/OrderDocument.java
@@ -0,0 +1,53 @@
+package com.example.backend.domain;
+
+import java.time.LocalDate;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
+@Table(name = "order_document")
+public class OrderDocument {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, unique = true, updatable = false)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @Column(name = "document_type", length = 50, nullable = false)
+    private String documentType;
+
+    @Column(name = "document_url", length = 255, nullable = false)
+    private String documentUrl;
+
+    @CreationTimestamp
+    @Column(name = "upload_date", nullable = false, updatable = false)
+    private LocalDate uploadDate;
+
+    @Column(name = "uploaded_by", length = 100, nullable = false)
+    private String uploadedBy;
+}
